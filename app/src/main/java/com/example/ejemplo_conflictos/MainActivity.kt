e6522cc692c6a23fd33db042d5b6a4e16ee5b6e7
ceczzx <geraldo123villanueva@gmail.com>
2024-08-26 23:06:38 -0500

    Update MainActivity.kt
    He agreado, Como estas

--- app/src/main/java/com/example/ejemplo_conflictos/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo_conflictos/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             EjemploConflictosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android, Como estas",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
